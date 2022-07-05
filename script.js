2c7d8c76e6c8b3bc891bec0bb667809ae1b57e67
Mattis <mtbtn@pm.me>
2022-07-05 12:56:05 +0200

    v1.1
    added light/dark theme switch

---
+++ script.js
@@ -0,0 +1,21 @@
+function lockScroll() {
+    document.body.classList.toggle('lock-scroll');
+}
+
+function removeHash() {
+    setTimeout(function(){
+        history.replaceState("", document.title, window.location.pathname);
+    }, 1);
+}
+
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    const checkbox = document.getElementById('emoji-checkbox');
+
+    if (event.currentTarget.checked) {
+        checkbox.innerText = "☀️";
+    } else {
+        checkbox.innerText = "🌑";
+    }
+ }
